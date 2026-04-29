ee22d534ffef4cf8c2ff7d8be76e9d30bf4dffc8
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-04-29 10:26:18 +0330

    inital commit

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function ProgressBar({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const time = setTimeout(onTimeout, timeout);
+    return () => {
+      clearTimeout(time);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress value={remainingTime} max={timeout} />;
+}
